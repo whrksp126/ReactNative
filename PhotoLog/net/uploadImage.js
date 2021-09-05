2b5e7a07cb132b5380ea94747d5231ea5ccb083f
whrksp126 <whrksp126@naver.com>
2021-09-06 00:00:13 +0900

    two

--- PhotoLog/net/uploadImage.js
+++ PhotoLog/net/uploadImage.js
@@ -1,54 +1,66 @@
-// import ImagePicker from 'react-native-image-picker';
-// import axios from 'axios';
-// import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import ImagePicker from 'react-native-image-picker';
+import axios from 'axios';
+import React from 'react'
 
-// async function uploadiImage() {
-//   const [imageUri, setImageUri] = React.useState('');
+import {
+  PermissionsAndroid
+} from "react-native";
+import {
+  launchCamera,
+  launchImageLibrary
+} from 'react-native-image-picker';
 
-//   const options = {
-//     title: 'Select Avatar',
-//     customButtons: [{
-//       name: 'fb',
-//       title: 'Choose Photo from Facebook'
-//     }],
-//     storageOptions: {
-//       skipBackup: true,
-//       path: 'images',
-//     },
-//   };
-  
-//   const config = {
-//     headers: {
-//       Authorization: 'Client_ID f08d16ebbbd26d2'
-//     }
-//   }
+const requestCameraPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA, {
+        title: "Cool Photo App Camera Permission",
+        message: "Cool Photo App needs access to your camera " +
+          "so you can take awesome pictures.",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK"
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("You can use the Camera");
+    } else {
+      console.log("Camera permission denied");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
+// requestCameraPermission()
 
-//   //promisify
-//   return new Promise((resolve, reject) => {
-//     ImagePicker.showImagePicker(options, (response) => {
-//       console.log('Response = ', response);
-
-//       if (response.didCancel) {
-//         console.log('User cancelled image picker');
-//         //  reject( {message : 'User cancelled image picker'} );
-//       } else if (response.error) {
-//         console.log('ImagePicker Error: ', response.error);
-//         reject({message: response.error});
-//       } else {
-//         const params = new FormData();
-//         params.append('image', response.data)
-//         axios.post('https://api.imgur.com/3/image', params, config)
-//           .then(response => {
-//             resolve(response.data.data.link);
-//           })
-//           .catch(error => {
-//             reject({
-//               message: error.response.data.data.error
-//             });
-//           })
-//       }
-//     });
-//   })
-// }
-
-// export default uploadiImage;
+const config = {
+  headers: {
+    Authorization: 'Client-ID f08d16ebbbd26d2',
+  }
+}
+async function uploadImage() {
+  return new Promise((resolve) => {
+    const config = {
+      headers: {
+        Authorization: 'Client-ID 21e459cc3b8bb2c',
+      }
+    }
+    launchImageLibrary({
+        mediaType: 'photo',
+        quality: 1,
+        saveToPhotos: true,
+        includeBase64: true,
+    },
+      res => {
+        const params = new FormData();
+        params.append('image', res)
+        axios.post('https://api.imgur.com/3/image', params, config)
+          .then(res => {
+            resolve( response.data.data.link );
+          })
+        console.log(res);
+      }
+    )
+  })
+}
+export default uploadImage;
--- PhotoLog/pages/Form.js
+++ PhotoLog/pages/Form.js
@@ -1,142 +1,60 @@
 import React from 'react'
 import styled from 'styled-components/native'
-import {  PermissionsAndroid} from "react-native";
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import uploadImage from '../net/uploadImage'
 
 
 const Title = styled.Text`
-  font-size: 36px;
+    font-size: 36px;
 `;
 
 const Button = styled.Button``;
 
 const Image = styled.Image`
-  width: 100%;
-  height: 360px;
+    width: 100%;
+    height: 360px;
 `;
 
 const Input = styled.TextInput`
-  width: 100%;
-  font-size: 16px;
-  padding: 4px;
-  border: 1px solid #e5e5e5;
+    width: 100%;
+    font-size: 16px;
+    padding: 4px;
+    border: 1px solid #e5e5e5;
 `;
 
 function Component() {
-  // const [imageUri, setImageUri] = React.useState('');
-  // const [keyword, setKeyword] = React.useState('')
-
-  // const openCamera = () => {
-  //   let options={
-  //     storageOption : {
-  //       path: 'images',
-  //       mediaType: 'photo',
-  //       quality: 1,
-
-  //     },
-  //     includeBase64: true,
-  //   };
-
-  //   launchCamera(options, response => {
-  //     console.log('Response =', response);
-  //     if (response.didCancel){
-  //       console.log('User cancelled image picker');
-  //     } else if (response.error){
-  //       console.log('ImagePicker Error:', response.error);
-  //     } else if (response.customButton) {
-  //       console.log('User tapped custom button:', response.customButton);
-  //     } else {
-  //       const source = {uri: 'data:image/jpeg;base64' + response.base64 };
-  //       setImageUri(source);
-  //     }
-  //   });
-  // }
-
-  // const openGallery = () => {
-  //   let options={
-  //     storageOption : {
-  //       mediaType: 'photo',
-  //       path: 'images',
-  //     },
-  //     includeBase64: true,
-  //   };
-
-  //   launchImageLibrary(options, response => {
-  //     console.log('Response =', response);
-  //     if (response.didCancel){
-  //       console.log('User cancelled image picker');
-  //     } else if (response.error){
-  //       console.log('ImagePicker Error:', response.error);
-  //     } else if (response.customButton) {
-  //       console.log('User tapped custom button:', response.customButton);
-  //     } else {
-  //       const source = {uri: 'data:image/jpeg;base64' + response.base64 };
-  //       setImageUri(source);
-  //     }
-  //   });
-  // }
-
- 
-  const requestCameraPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: "Cool Photo App Camera Permission",
-          message:
-            "Cool Photo App needs access to your camera " +
-            "so you can take awesome pictures.",
-          buttonNeutral: "Ask Me Later",
-          buttonNegative: "Cancel",
-          buttonPositive: "OK"
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("You can use the camera");
-      } else {
-        console.log("Camera permission denied");
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
+  const [url, setUrl] = React.useState( null );
+  const [hashtags, setHashtags] = React.useState('');
+  
+  const selectImage = function() {
+    uploadImage()
+      .then( url => {
+        setUrl( url );
+      } ).catch(error=>{
+          alert( error.message );
+      } );
+  }
 
   return (
     <>
-
-      <Button title="Open Camera" onPress={ //openCamera
-        async () => {
-          await launchCamera({
-            mediaType:'photo', 
-            quality:1, 
-            saveToPhotos: true
-          }, 
-          response => {
-            requestCameraPermission
-              .then
-                if (response) {
-                console.log('response',response);
-          
-        
-      }})}}/>
-
-      {/* <Image 
-        source={imageUri}
-        style={{ 
-          height: 100,
-          width: 100,
-          borderRadius: 100,
-          borderWidth: 2,
-          borderColor: 'black',
-        }}
-      />
-
+      <Button title="이미지 선택" onPress={ selectImage } />
+      <Image source={{ uri: url }}/>
       <Input 
-        placeholder="#해시태그" 
-        value={keyword} 
-        onChangeText={value => setKeyword(value)} 
+        placeholder="#해시태그"
+        value={ hashtags }
+        onChangeText={ value => setHashtags( value ) }
       />
-      <Button title="저장" onPress={() => {}}/> */}
+      <Button 
+        title="저장" 
+        // onPress={ () => {
+        //   storage.append( {
+        //     url,
+        //     hashtags,
+        //   })
+        //   .then( () => {
+        //     props.navigation.goBack();
+        //   });
+        // }}
+      />
     </>
   )
 }
